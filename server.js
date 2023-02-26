ad2f0c7bed3df4afd5e8663bab11f3e36e79877a
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-02-26 19:09:09 +0100

    second commit

--- server.js
+++ server.js
@@ -41,17 +41,12 @@ app.use('/projects', projectRoutes);
 app.use('/messages', messageRoutes);
 
 // ------------------DEPLOYMENT--------------
-const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running')
-    });
-};
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 // ------------------DEPLOYMENT--------------
 
 // #error handling middlewares for api calls
